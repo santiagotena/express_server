8153c1bc4cb1ee2b21dc4b4cd432c871920c48ea
Santiago Tena Hernandez <stenahe@gmail.com>
2024-11-14 14:10:08 +0100

    solve until 3.8

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+morgan.token('req-body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
+
 let persons = [
 	{ 
 		"id": "1",
