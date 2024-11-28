45f8a88872895d65184fb8bdc7e11f8a1f2bbd60
Santiago Tena Hernandez <stenahe@gmail.com>
2024-11-28 20:56:25 +0100

    solve until 3.21

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ morgan.token('req-body', (req) => {
 })
 
 const errorHandler = (error, request, response, next) => {
-	console.error(error.message)
+  console.error(error.message)
 
-	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' })
-	}
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-	next(error)
+  next(error)
 }
 
 const unknownEndpoint = (request, response) => {
@@ -37,26 +39,25 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-		response.json(persons)
-	})
+  Person.find({})
+		.then(persons => {
+			response.json(persons)
+		})
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body
-
-	if (body.name === undefined || body.number === undefined) {
-    return response.status(400).json({ error: 'incomplete entry' })
-  }
 
 	const person = new Person({
 		name: body.name,
 		number: body.number,
 	})
 
-	person.save().then(savedPerson => {
-		response.json(savedPerson)
-	})
+	person.save()
+		.then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -80,18 +81,17 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-	const body = request.body
-	
-	const person = {
-		name: body.name,
-		number: body.number,
-	}
+	const { name, number } = request.body
 
-	Person.findByIdAndUpdate(request.params.id, person, {new: true})
+	Person.findByIdAndUpdate(
+		request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+	)
 		.then(updatedPerson => {
 			response.json(updatedPerson)
 		})
-		// .catch(error => next(error))
+		.catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
--- models/person.js
+++ models/person.js
@@ -1,29 +1,42 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then(() => {
+    console.log('connected to MongoDB');
   })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
-})
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number! It must be in the format XX-YYYYYYY or XXX-YYYYYYYY.`,
+    },
+  },
+});
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
