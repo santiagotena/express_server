0ce2d351dc8b72b73785ac292a703dfdbcde8c72
Santiago Tena Hernandez <stenahe@gmail.com>
2024-11-30 17:23:33 +0100

    improve validation

--- models/person.js
+++ models/person.js
@@ -24,7 +24,7 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-\d{5,}$/.test(v)
+        return /^(\d{3}-\d{5,}|\d{2}-\d{6,})$/.test(v).test(v)
       },
       message: (props) => `${props.value} is not a valid phone number! It must be in the format XX-YYYYYYY or XXX-YYYYYYYY.`,
     },
